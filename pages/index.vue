351c766941942f37cb1f400600c578d9d79a191b
Melodie <melodie.janvier@mail-ecv.fr>
2024-04-12 09:22:00 +0200

    clean useless files

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,6 @@
         <Counter></Counter>
         <Username></Username>
         <Parent></Parent>
-        <div class="image">
-            <img style="width:250px;" src="~assets/fred.jpg" alt="fred"/>
-        </div>
         <li><NuxtLink :to="{name: 'blog-slug', params: {slug: 'test'}}">Blog usage</NuxtLink></li>
         <li><NuxtLink :to="{name: 'dato-fetch'}">Dato usage</NuxtLink></li>
     </div>
